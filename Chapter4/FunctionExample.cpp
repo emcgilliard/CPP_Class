d63b7742612f5dcf18cf4da6540d496334a5437b
Eric M <emcgilliard@gmail.com>
2018-04-10 12:37:56 -0600

    Create FunctionExample.cpp

---
+++ Chapter4/FunctionExample.cpp
@@ -0,0 +1,40 @@
+/******************************************************************************
+
+This program demonstrates how to create a function
+
+*******************************************************************************/
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+
+using namespace std;
+
+double total_cost(int number_par, double price_par);        //function declaration
+//computes the total cose, including 5% sales tax, 
+//on number_par items at a cost of price_par each
+
+int main()
+{
+
+    double price, bill; 
+    int number; 
+    
+    cout << "Enter the number of items purchased: "; 
+    cin  >> number; 
+    cout << "Enter the price per item $";
+    cin  >> price; 
+    
+    bill - total_cost(number, price);       //function call
+    
+    cout.setf(ios::fixed);
+    cout.setf(ios::showpoint);
+    cout.precision(2);
+    cout << number << " items at "
+         << "$" << price << " each.\n"
+         << "Final bill, including tax, is $" << bill
+         << endl;
+         
+    return 0;
+}

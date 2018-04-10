bc79a1222474686f2c5c075a1ade84a72a785fbd
Eric M <emcgilliard@gmail.com>
2018-04-09 21:39:37 -0600

    Rename DogHouse_Calculator to DogHouse_Calculator.cpp

---
+++ Chapter4/DogHouse_Calculator.cpp
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+This program computes the size of a dog house
+
+*******************************************************************************/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    const double COST_PER_SQ_FT = 10.50;
+    double budget, area, length_side;
+    
+    cout << "Enter the amount budgeted for your dog house $";
+    cin >> budget;
+    
+    area = budget / COST_PER_SQ_FT;
+    length_side = sqrt(area);
+    
+    cout.setf(ios::fixed);
+    cout.setf(ios::showpoint);
+    cout.precision(2);
+    cout << "For a price of $" << budget << endl
+         << "I can build you a dog house\n"
+         << "that is " << length_side
+         << " feet on each side.\n";
+         
+
+    return 0;
+}

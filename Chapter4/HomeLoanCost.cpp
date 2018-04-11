79aa9f522a185213462cf9d9124a660b8c2bbe7f
Eric M <emcgilliard@gmail.com>
2018-04-10 20:50:45 -0600

    Update HomeLoanCost.cpp

--- Chapter4/HomeLoanCost.cpp
+++ Chapter4/HomeLoanCost.cpp
@@ -17,20 +17,24 @@ using namespace std;
 const double TAX_RATE = 0.35;
 
 //Function declarations
-double getPrice();                                  //asks the user for the cost of the home
-double getDownpayment();                            //asks the user for the down payment amount
-double getBalance(double price, double downPayment);//get the initial laon balance
+double getPrice();                                   //asks the user for the cost of the home
+double getDownpayment();                             //asks the user for the down payment amount
+double getBalance(double price, double downPayment); //get the initial laon balance
+double setCost(double initialLoan);
 
 
 int main()
 {
     
-    double initialLoan, price, downPayment;
+    double initialLoan, price, downPayment, cost;
+    char loop;
     
     //Welcome message
     cout << "Welome to the mortgage cost calculator!\n"
          << "THis program will calculate the annual after-tax cost of a new home.\n";
-    
+ 
+do 
+{
     //Get the input from the user
     price = getPrice();
     downPayment=  getDownpayment();
@@ -38,9 +42,18 @@ int main()
     //Get the initial loan balance
     initialLoan = getBalance(price, downPayment);
     
-    //
+    //determine the cost of the loan
+    cost = setCost(initialLoan);
     
     cout<<"Hello World";
+    
+    
+    //Ask if the user wants to repeat the calculation
+    cout << "Do you want to do another calculation? (Y for Yes)";
+    cin >> loop;
+} 
+
+while (loop == 'Y' || loop == 'y');
 
     return 0;
 }
@@ -79,6 +92,17 @@ double getBalance(double price, double downPayment)
     return balance;
 }
 
+/******************************************************************************
+getCost determines the cost of the loan
+*****************************************************************************/
+double setCost(double initialLoan)
+{
+    double principal_pmt, interest_pmt, cost;
+    principal_pmt = initialLoan * 0.03;
+    interest_pmt = initialLoan * 0.06;
+    cost = principal_pmt + interest_pmt;
+    return cost; 
+}
 
 
 

cc9d01fc8137db0f75007b309da038bbf5978ad4
Eric M <emcgilliard@gmail.com>
2018-04-29 17:57:39 -0600

    Create PointerExample.cpp

---
+++ Chapter8_Pointers/PointerExample.cpp
@@ -0,0 +1,31 @@
+/******************************************************************************
+This program demonstrates the user of pointers
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int *p1, *p2; 
+    
+    p1 = new int; 
+    *p1 = 42; 
+    p2 = p1;
+    cout << "*p1 == " << *p1 <<endl;
+    cout << "*p2 == " << *p2 << endl;
+    
+    *p2 = 53; 
+    cout << "*p1 == " << *p1 << endl; 
+    cout << "*p2 == " << *p2 << endl; 
+    
+    p1 = new int; 
+    *p1 = 88; 
+    cout << "*p1 == " << *p1 << endl; 
+    cout << "*p2 == " << *p2 << endl; 
+    cout << "hope you got the point of this example!\n"; 
+
+    return 0;
+}
